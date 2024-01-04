8507092d58d7302396dd48d56ac169e5d62b8c6d
sijalica <igorradivojevic996@gmail.com>
2024-01-04 15:30:36 +0100

    initial

---
+++ mssc-beer-service/src/test/java/com/course/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
