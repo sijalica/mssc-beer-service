a28142c0c2acb5decac6e21be531c7206808741f
sijalica <igorradivojevic996@gmail.com>
2024-01-07 00:34:42 +0100

    adding repository for beer objects, closes #5

---
+++ mssc-beer-service/src/main/java/com/course/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.course.msscbeerservice.repositories;
+
+import com.course.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
